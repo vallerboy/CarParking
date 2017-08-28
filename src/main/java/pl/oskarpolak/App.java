0d822ea3fdd741bf514ddc33f98e8539a139dfa1
Oskar Polak <oskarbank@gmail.com>
2017-08-28 12:59:02 +0200

    Initial commit

---
+++ src/main/java/pl/oskarpolak/App.java
@@ -0,0 +1,17 @@
+package pl.oskarpolak;
+
+/**
+ * Created by Lenovo on 28.08.2017.
+ */
+public class App {
+    public static void main(String[] args) {
+       new App();
+    }
+
+    Connector connector = Connector.getInstance();
+
+    public App() {
+
+    }
+}
+
